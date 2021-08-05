df57f49597a35d1942dc2afbe81bb546dc3e780e
Yuriko Hijikata <akanesasu3@gmail.com>
2021-08-05 15:32:50 +0900

    Initial Commit

---
+++ quoteClipper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quoteClipper
+//
+//  Created by Yuriko Hijikata on 8/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quoteClipper/quoteClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  quoteClipperApp.swift
+//  quoteClipper
+//
+//  Created by Yuriko Hijikata on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct quoteClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
